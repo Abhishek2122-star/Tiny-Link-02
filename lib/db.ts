6369fa310f1a9b31f7109ba2b8b8aa8b29555bc8
Abhishek2122-star <abhisheklad963@gmail.com>
2025-11-22 23:46:05 +0530

    Updated code

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,22 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
 
-const pool = new Pool({
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// export async function query(text: string, params?: any[]) {
+//   try {
+//     return await pool.query(text, params);
+//   } catch (error) {
+//     console.error('DB Error:', error);
+//     throw error;
+//   }
+// }
+
+
+import { Pool } from "pg";
+
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
-
-export async function query(text: string, params?: any[]) {
-  try {
-    return await pool.query(text, params);
-  } catch (error) {
-    console.error('DB Error:', error);
-    throw error;
-  }
-}
